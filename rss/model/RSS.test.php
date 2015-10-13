218ddb5f4c1586c97a13fc8ab10b6453de2bb025
gorkat <tgorka1@gmail.com>
2015-10-13 20:32:29 +0200

    Modif jeu de test :) Le fichier RSS.test.php affiche désormais chaque nouvelle sous forme d'un tableau suivi de l'image téléchargée correspondante !

--- rss/model/RSS.test.php
+++ rss/model/RSS.test.php
@@ -11,20 +11,24 @@ echo $rss->titre()."\n";
 echo "dernière mise à jour le : ".$rss->date()."\n";
 
 $it=0;
+echo "<html>";
+echo "<head><title>RSS</title>";
+echo "<style>td {border : 1px solid black; padding : 5px;} table {border-collapse : collapse;} img {width : 30%; height : 30%;}</style></head>";
+echo "<body>";
 
 foreach ($rss->nouvelles() as $node) {
 
-	if ($it<1) {
+	//if ($it<4) {
 		$nouvelle = new Nouvelles;
 		$nouvelle->update($node);
-/*		echo "\n".$nouvelle->titre()."\n";
-		echo $nouvelle->pubDate()."\n";;
-		echo $nouvelle->description()."\n";
-		echo $nouvelle->lien()."\n";
-		echo $nouvelle->id()."\n";
-*/
-	}
-	$it++;
+		echo "<table>";
+		echo "<tr><th>".$nouvelle->titre()."</th></tr>";
+		echo "<tr><td>".$nouvelle->pubDate()."</td></tr>";
+		echo "<tr><td>".$nouvelle->description()."</td></tr>";
+		echo "<tr><td>".$nouvelle->lien()."</td></tr>";
+		echo '</table><img src="'.$nouvelle->image().'">';
+	//}
+	//$it++;
 }
-
+echo "</body></html>";
 ?>
